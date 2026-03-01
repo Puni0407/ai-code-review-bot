92782942497cc941658cb657a677fa3cb854d001
Punithan <punithan0704@gmail.com>
2026-03-01 14:08:41 +0530

    Format sample.js

--- src/sample.js
+++ src/sample.js
@@ -3,23 +3,20 @@
 // ✅ Good practice: secrets via env, not hardcoded
 function login(user) {
   //const password = process.env.PASSWORD;
-const password = "123456";
+  const password = '123456';
 
-function check(a, b) {
-  if (a == b) {
-    console.log("Equal");
+  function check(a, b) {
+    if (a == b) {
+      console.log('Equal');
+    }
   }
-}
   //if (user === null) {
-    //console.log('user not found');
+  //console.log('user not found');
   //}
-
-  
 
   // Using the variable so ESLint won't warn
   return password ? 'ok' : 'missing password';
 }
-
 
 // Call function so it's not unused
 login('admin');
